fa91e902f7fb43a150bef0bb4d1794c098546427
Vivien <fusco.vivien@gmail.com>
2015-01-23 23:10:21 +0100

    Added Player Controller & Debug Scenes/debug.unity Modified InputManager.asset

---
+++ UnityProject/Assets/DoSth/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public string whichPlayer;
+	public float speed;
+	public bool isDashing = false; 
+
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+
+		if (Input.GetAxisRaw (whichPlayer+"_Vertical") != 0) {
+			this.transform.Translate(Vector3.right * speed * Time.deltaTime * Input.GetAxisRaw (whichPlayer+"_Vertical") , Space.World);
+		}
+		if (Input.GetAxisRaw (whichPlayer+"_Horizontal") != 0) {
+			this.transform.Translate(Vector3.forward * speed * Time.deltaTime * Input.GetAxisRaw (whichPlayer+"_Horizontal") , Space.World);
+		}
+
+		if (Input.GetButton (whichPlayer + "_A")) {
+			isDashing = true;
+		} 
+		else {
+			isDashing = false;
+		}
+	}
+
+	public void OnCollisionEnter(Collision other){
+		if (other.transform.tag == "Player" && this.isDashing) {
+
+		}
+	}
+}
