c0690d9eb68fd69b638d126234b80a1b4b0946d4
sylafrs <sylaf@hotmail.fr>
2015-01-25 10:39:32 +0100

    RULE THE BEER !!

---
+++ UnityProject/Assets/DoSth/Scripts/PickableBeer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickableBeer : PickageItemRange 
+{
+	[HideInInspector]
+	public Player Owner;
+
+	[HideInInspector]
+	public ProjectorLookAt Projector;
+
+	public override bool CanTake
+	{
+		get
+		{
+			return Owner == null;
+		}
+	}
+
+	void OnDestroy()
+	{
+		GameObject.Destroy(Projector.gameObject);
+
+		if(Owner)
+			Owner.gameObject.SendMessage("OnBeerDestroyed");
+	}
+}
---
+++ UnityProject/Assets/DoSth/Scripts/Rules/RuleTheBeer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RuleTheBeer : Rule {
+
+	private PickableBeer Beer;
+
+	public override string Description
+	{
+		get { return "Rule the beer!"; }
+	}
+
+	public override void Prepare(GameManager manager)
+	{
+		Beer			= (GameObject.Instantiate(this.transform.FindChild("Beer").gameObject) as GameObject).GetComponent<PickableBeer>();
+		Beer.Projector	= (GameObject.Instantiate(this.transform.FindChild("Projector").gameObject) as GameObject).GetComponent<ProjectorLookAt>();
+
+		if (Beer.Projector == null)
+			throw new UnityException("damn");
+	}
+
+	public override void GameOver()
+	{
+		GameObject.Destroy(Beer.gameObject);
+		base.GameOver();
+	}
+
+	public override Player[] GetWinners()
+	{
+		if (Beer.Owner)
+		{
+			return new Player[] { Beer.Owner };
+		}
+
+		return new Player[0];
+	}
+}
