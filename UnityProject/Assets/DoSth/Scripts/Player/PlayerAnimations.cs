02899e30f3fdbca3679fa1ea1a919b193a4c8c4a
Echonia <m.sauvage@hotmail.com>
2015-01-25 01:43:08 +0100

    Added NPC Added IntegrationScene 2 Added Animation Script Modified Arena Prefab

---
+++ UnityProject/Assets/DoSth/Scripts/Player/PlayerAnimations.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnimations : MonoBehaviour {
+
+	private Player player;
+	private string prevAnimation;
+
+	public Animator myAnimator;
+
+	void Start () {
+
+		player = this.GetComponent<Player>();
+
+	}
+	
+	void Update () {
+	
+	}
+
+	void OnMove()
+	{
+//		walk.enableEmission = !player.IsDashing;
+		if(player.IsDashing){
+			UseAnimation("Dash");
+		}
+		else{
+			UseAnimation("Run");
+		}
+	}
+	
+	void OnIdle()
+	{
+		UseAnimation("Idle");
+	}
+	
+	void OnDeath()
+	{
+
+	}
+	
+	void OnPushed()
+	{
+		UseAnimation("Bump");
+
+	}
+
+	void UseAnimation(string str)
+	{
+		if(prevAnimation != str)
+		{
+			myAnimator.CrossFade(str, 0.1f);
+			prevAnimation = str;
+		}
+	}
+}
