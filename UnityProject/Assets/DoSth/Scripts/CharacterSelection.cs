d43ffc41be6762d6064c6d82c2d59101386c59c6
Esefen <romain.soual.pro@gmail.com>
2015-01-25 02:32:09 +0100

    MAJ CharacterSelection : lights

--- UnityProject/Assets/DoSth/Scripts/CharacterSelection.cs
+++ UnityProject/Assets/DoSth/Scripts/CharacterSelection.cs
@@ -6,8 +6,7 @@ using XInputDotNetPure;
 public class CharacterSelection : MonoBehaviour {
 
 	bool[] selectedCharacters = new bool[4];
-	int nbDots = 0, nbPlayersSelected = 0;
-	Text[] displayFeedback;
+	int nbPlayersSelected = 0;
 	GameObject[] players;
 	string[] playerName = new string[4];
 	public Light[] spotlights;
@@ -15,15 +14,17 @@ public class CharacterSelection : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		players = GameObject.FindGameObjectsWithTag("Player");
-		displayFeedback = GetComponentsInChildren<Text>();
+		if (Input.GetJoystickNames().GetLength(0) < 4) {
+			if (!GamePad.GetState(PlayerIndex.One).IsConnected) Debug.LogError("NO.");
+			if (!GamePad.GetState(PlayerIndex.Two).IsConnected) Debug.LogError("NO.");
+			if (!GamePad.GetState(PlayerIndex.Three).IsConnected) GameObject.Find("Player_3").SetActive(false);
+			if (!GamePad.GetState(PlayerIndex.Four).IsConnected) GameObject.Find("Player_4").SetActive(false);
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		int nbGamepads = Input.GetJoystickNames().GetLength(0);
-		//selectedCharacters = new bool[nbGamepads];
-		//Debug.Log(GamePad.GetState(PlayerIndex.One).Buttons.A);
-		if (nbPlayersSelected < 4) {
+		if (nbPlayersSelected < Input.GetJoystickNames().GetLength(0)) {
 			if (GamePad.GetState(PlayerIndex.One).Buttons.Start == ButtonState.Pressed && !selectedCharacters[0]) {
 				selectedCharacters[0] = true;
 				players[nbPlayersSelected].GetComponent<Player>().Id = 1;
@@ -73,11 +74,9 @@ public class CharacterSelection : MonoBehaviour {
 				nbPlayersSelected++;
 			}
 		}
+		else StartCoroutine(startTheGame());
 
 		/*
-
-		for (int i=0 ; i < nbPlayersSelected ; i++) {
-		}
 		for (int i=0 ; i < nbGamepads ; i++) {
 			if (!selectedCharacters[i]) {
 				displayFeedback[i].text = "Waiting";
@@ -90,5 +89,10 @@ public class CharacterSelection : MonoBehaviour {
 			else displayFeedback[i].text = "Ok";
 		}
 		*/
+	}
+
+	IEnumerator startTheGame () {
+		yield return new WaitForSeconds(3.0f);
+		Application.LoadLevel("debug_A");
 	}
 }
