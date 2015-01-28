6fbb8bd70b61e1da25056f57afbad9cd783486a3
sylafrs <sylaf@hotmail.fr>
2015-01-28 01:05:51 +0100

    scene clean finished.

--- UnityProject/Assets/DoSth/Scripts/TableauDesScores.cs
+++ UnityProject/Assets/DoSth/Scripts/TableauDesScores.cs
@@ -5,41 +5,40 @@ using System.Collections;
 public class TableauDesScores : MonoBehaviour {
 
 	public static int[] scores = new int[4];
-	int indexWinner = -1;
+    int maxScore = 0;
 	
-	void Awake () {
-		if (Application.loadedLevelName == "FinalScore") DontDestroyOnLoad(gameObject);
+	void Start ()
+    {
+        int maxScore = 0;
+        for (int i = 0; i < scores.Length; i++)
+        {
+            if (scores[i] > maxScore)
+            {
+                maxScore = scores[i];
+            }
+        }
+
+	    for (int i = 0; i < 4; i++)
+        {
+            if (scores[i] < maxScore)
+                Looser(i);
+        }
+		
 	}
 
-	// Use this for initialization
+    void Looser(int id)
+    {
+        GameObject go = GameObject.Find("Player_" + (id + 1));
+        if (go)
+        {
+            PlayerAnimations pa = go.GetComponent<PlayerAnimations>();
+            if(pa)
+                pa.UseAnimation("Loser");
+        }
+    }
 
-	void Start () {
-		recordPlayersScore();
-		if (Application.loadedLevelName == "FinalScore") {
-			if (indexWinner != 1) GameObject.Find("Player_1").GetComponent<PlayerAnimations>().UseAnimation("Loser");
-			if (indexWinner != 2) GameObject.Find("Player_2").GetComponent<PlayerAnimations>().UseAnimation("Loser");
-			if (indexWinner != 3) GameObject.Find("Player_3").GetComponent<PlayerAnimations>().UseAnimation("Loser");
-			if (indexWinner != 4) GameObject.Find("Player_4").GetComponent<PlayerAnimations>().UseAnimation("Loser");
-			for (int i=0 ; i < 0 ; i++) {
-				transform.GetChild(i).GetComponent<Text>().enabled = true;
-				transform.GetChild(i).GetComponent<Text>().text = scores[i].ToString();
-			}
-		}
-	}
-
-	void recordPlayersScore ()
-	{
-		indexWinner = 1;
-		int maxScore = 0;
-		for(int i = 0; i < scores.Length; i++)
-		{
-			if (scores[i] > maxScore)
-			{
-				maxScore = scores[i];
-				indexWinner = (i + 1);
-			}
-		}
-	}
-
-
+    public void MainMenu()
+    {
+        Application.LoadLevel(0);
+    }
 }
